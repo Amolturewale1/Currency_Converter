9a41d40da95281e4ccd6312d3bc6ffcffe343a08
Amol Turewale <133583961+Amolturewale1@users.noreply.github.com>
2024-05-20 22:43:48 +0530

    index file added

---
+++ app.js
@@ -0,0 +1,71 @@
+const BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("form button");
+
+const fromCurr = document.querySelector(".from select");
+const toCurr =document.querySelector(".to select");
+
+const msg=document.querySelector(".msg");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption =document.createElement("option");
+        newOption.innerHTML=currCode;
+        newOption.value=currCode;
+
+        if(select.name  === "from" && currCode ==="USD"){
+            newOption.selected="selected";
+        }else if(select.name  === "to" && currCode ==="INR"){
+            newOption.selected="selected";
+        }
+        select.append(newOption);
+
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target)
+    })
+
+}
+
+
+const updateFlag =(element)=>{
+    let currCode =element.value;
+   
+    let countryCode=countryList[currCode];
+  
+    let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img= element.parentElement.querySelector("img");
+    img.src=newSrc;
+}
+
+btn.addEventListener("click", async(evt)=>{
+    evt.preventDefault();
+    let amount =   document.querySelector(".amount input");
+    let amtValue=amount.value.trim();
+    amtValue =parseFloat(amtValue); 
+    if(amtValue ==="" || amtValue <1){
+        amtValue=1;
+        amount.value="1";
+    }
+    
+    
+    console.log(fromCurr.value,toCurr.value);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    try {
+        let response = await fetch(URL);
+        if (!response.ok) throw new Error('Network response was not ok');
+        let data = await response.json();
+        let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+        console.log(`Exchange rate from ${fromCurr.value} to ${toCurr.value}: ${rate}`);
+        
+        let convertedAmount = amtValue * rate;
+        console.log(`Converted amount: ${convertedAmount}`);
+
+        msg.innerText =`${amtValue} ${fromCurr.value} = ${convertedAmount} ${toCurr.value}`;    
+        
+        // Optionally display the result in the UI
+        // document.querySelector('.result').textContent = `Converted amount: ${convertedAmount}`;
+    } catch (error) {
+        console.error('Fetch error: ', error);
+    }
+});
